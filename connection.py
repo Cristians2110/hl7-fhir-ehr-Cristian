ec91467dea79c23ced9622cc1802af45f53fd482
Cristians2110 <cristian.capera@urosario.edu.co>
2025-03-27 00:03:11 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -6,6 +6,7 @@ def connect_to_mongodb(db_name, collection_name):
     uri = "mongodb+srv://Cristian2110:abcdef123@cluster0.tdjsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = SamplePatientService
-    collection = patients
+    collection_name = patients
+    collection = connect_to_mongodb(uri, db_name, collection_name)
     return collection
 
