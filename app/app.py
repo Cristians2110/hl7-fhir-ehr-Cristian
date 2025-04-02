7a038e5623532c988092fd1be7ebcf4dd8f3b787
Cristians2110 <cristian.capera@urosario.edu.co>
2025-04-02 15:59:47 -0500

    d

--- app/app.py
+++ app/app.py
@@ -27,7 +27,8 @@ async def get_patient_by_id(patient_id: str):
 
 
 @app.get("/patient", response_model=dict)
-async def get_patient_by_identifier(system:str, value:str):
+async def get_patient_by_identifier(system: str, value: str):
+    print("solicitud datos:",system,value)
     status,patient = GetPatientByIdentifier(system, value)
     if status=='success':
         return patient  # Return patient
