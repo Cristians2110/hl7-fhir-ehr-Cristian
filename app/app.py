fb268fee467451ece518fb2f83b219f21f6304a5
Cristians2110 <cristian.capera@urosario.edu.co>
2025-04-05 22:28:32 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,27 +7,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
-@app.post("/enviar_orden")
-async def enviar_orden(
-    id_paciente: str = Form(...),
-    tipo_procedimiento: str = Form(...),
-    motivo: str = Form(...),
-    prioridad: str = Form(...),
-    comentarios: str = Form(None)
-):
-    return {
-        "mensaje": "Orden recibida correctamente",
-        "id_paciente": id_paciente,
-        "tipo_procedimiento": tipo_procedimiento,
-        "motivo": motivo,
-        "prioridad": prioridad,
-        "comentarios": comentarios
-    }
-    print("ServiceRequest recibido:", service_request)
-    return JSONResponse(content={"status": "ok", "data": service_request})
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Permitir solo este dominio
