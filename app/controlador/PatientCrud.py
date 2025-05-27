3e2dd92198cf0ff83f137d4ac2f0cb5ff62daed9
Cristians2110 <cristian.capera@urosario.edu.co>
2025-05-27 16:29:18 -0500

    Update PatientCrud.py

--- app/controlador/PatientCrud.py
+++ app/controlador/PatientCrud.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from datetime import datetime
 import json
 
+# Conexión a la colección patients
 collection = connect_to_mongodb("SamplePatientService", "patients")
 
 def GetPatientById(patient_id: str):
@@ -15,30 +16,30 @@ def GetPatientById(patient_id: str):
             return "success", patient
         return "notFound", None
     except Exception as e:
-        return f"notFound", None
+        return "notFound", None
 
 def WritePatient(patient_dict: dict):
     try:
         pat = Patient.model_validate(patient_dict)
     except Exception as e:
-        return f"errorValidating: {str(e)}",None
+        return f"errorValidating: {str(e)}", None
     validated_patient_json = pat.model_dump()
     result = collection.insert_one(patient_dict)
     if result:
         inserted_id = str(result.inserted_id)
-        return "success",inserted_id
+        return "success", inserted_id
     else:
         return "errorInserting", None
 
 def GetPatientByIdentifier(patientSystem, patientValue):
-    try: 
+    try:
         patient = collection.find_one({"identifier.system": patientSystem, "identifier.value": patientValue})
         if patient:
-            patient["_id"] = str (patient["_id"])
+            patient["_id"] = str(patient["_id"])
             return "success", patient
         return "notfound", None
     except Exception as e:
-        return f"notfound", None 
+        return "notfound", None
 
 class ServiceRequest(BaseModel):
     patient_id: str
@@ -50,17 +51,25 @@ class ServiceRequest(BaseModel):
     status: str = "pending"
     created_at: datetime = datetime.utcnow()
 
-# Conectarse a la colección service_requests
-service_collection = connect_to_mongodb("SamplePatientService", "service_requests")
-
 def WriteServiceRequest(request_dict: dict):
     try:
+        tipo = request_dict.get("service_type", "").lower()
+
+        if tipo == "ris":
+            collection = connect_to_mongodb("SamplePatientService", "solicitudes")
+        elif tipo == "his":
+            collection = connect_to_mongodb("SamplePatientService", "citas")
+        elif tipo == "procedimiento":
+            collection = connect_to_mongodb("SamplePatientService", "procedimientos_clinicos")
+        else:
+            return "tipo_desconocido", None
+
         service = ServiceRequest(**request_dict)
-        result = service_collection.insert_one(service.model_dump())
+        result = collection.insert_one(service.model_dump())
         if result.inserted_id:
             return "success", str(result.inserted_id)
         else:
             return "errorInserting", None
     except Exception as e:
-        print("❌ Error en WriteServiceRequest:", str(e)) 
+        print("❌ Error en WriteServiceRequest:", str(e))
         return f"errorValidating: {str(e)}", None
